110948251f87d2a3db77548d3d06ccb8ef07348d
anshika1501 <anshikapriya12@gmail.com>
2026-03-27 01:41:04 +0530

    feat: initialize Django backend with core settings and stock data API endpoints.

--- backend/stock_api/settings.py
+++ backend/stock_api/settings.py
@@ -42,23 +42,20 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders',
     'pgvector.django',
     'stocks',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
